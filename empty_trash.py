5c0da103f534f85d3693944c5cabe372132d4aae
Alex Bogomolov <mail@abogomolov.com>
2019-05-16 09:06:24 +0300

    remove catch error

--- empty_trash.py
+++ empty_trash.py
@@ -22,15 +22,11 @@ def clean_system_trash():
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         if sys.argv[1] == "-d":
-            try:
-                command = 'tell app "Finder" to empty'
-                p = Popen(
-                    ["osascript", "-"], stdin=PIPE, stdout=PIPE, universal_newlines=True
-                )
-                stdout, stderr = p.communicate(command)
-                print("Trash is empty")
-            except Exception as e:
-                print(e)
-                print("trash is already empty")
+            command = 'tell app "Finder" to empty'
+            p = Popen(
+                ["osascript", "-"], stdin=PIPE, stdout=PIPE, universal_newlines=True
+            )
+            stdout, stderr = p.communicate(command)
+            print("all trash is gone")
     else:
         clean_system_trash()
