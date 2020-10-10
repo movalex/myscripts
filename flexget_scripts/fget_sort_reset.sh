8d0fe36ecd0895cd5ec1c5d7ce4e02eb35cb04ea
barney <barney@server.local>
2020-10-10 12:04:05 +0300

    simplify transmission sorter script

--- flexget_scripts/fget_sort_reset.sh
+++ flexget_scripts/fget_sort_reset.sh
@@ -10,8 +10,7 @@ TORRENTLIST=`$TRM $SERVER --list | sed -e '1d;$d;s/*//' | awk '{print $1}'`
 D=$(date)
 SORTER=false
 SEEDING=0
-UNCOMPLETED=0
-LOCATION="/Volumes/HARD1/TVTEMP"
+LOCATION="/Volumes/Elements/TVTEMP"
 SIZE=$(du -s $LOCATION | awk '{print $1}')
 
 echo `seq -f "-" -s '' ${#D}`
@@ -23,7 +22,6 @@ $TRM $SERVER --list | sed -e '1d;$d;s/*//'
 if ! [[ $TORRENTLIST ]];  then
     echo "Torrent list is empty"
     if [[ $SIZE -gt 600000 ]];  then
-        echo "setting sort to True"
         SORTER=true
     fi
 else
@@ -36,8 +34,6 @@ else
             STATE_LOCATION=` $INFO|grep "Location: $LOCATION" `
             STATE_STOPPED=` $INFO|grep "State: Stopped" `
             STATE_FINISHED=` $INFO|grep "State: Finished" `
-            STATE_IDLE=` $INFO|grep "State: Idle" `
-            STATE_SEEDING=` $INFO|grep "State: Seeding" `
 
             if [[ "$STATE_STOPPED" ]]; then
                  echo "trying to restart torrent"
@@ -49,52 +45,16 @@ else
             fi
 
             if [[ "$STATE_LOCATION" ]]; then
-                if [[ "$STATE_FINISHED" ]];  then
+                if [[ "$DL_COMPLETED" ]];  then
                     echo "Torrent #$TORRENTID (`$INFO |grep "Name" | sed -e 's/^ \{1,\}//g; s/Name: //g'`) is finished"
-                    echo "Removing torrent from list"
-                    $TRM $SERVER --torrent $TORRENTID --remove
-                    echo "Torrent #$TORRENTID has been removed" 
+                    $TRM $SERVER --torrent $TORRENTID --move "${LOCATION}/SORT/" && $TRM $SERVER --torrent $TORRENTID --remove
                     SORTER=true
-                elif [[ "$DL_COMPLETED" ]] && [[ ( "$STATE_SEEDING" || "$STATE_IDLE" ) ]]; then
-                SEEDING=$(( SEEDING + 1 ))
-                elif ! [[ "$DL_COMPLETED" ]];  then
-                    echo "Torrent #$TORRENTID is not completed. Ignoring."
-                    UNCOMPLETED=$(( UNCOMPLETED + 1 ))
-                fi
-            elif ! [[ $STATE_LOCATION ]]; then
-                if [[ "$STATE_FINISHED" ]];  then
-                    echo $STATE_FINISHED
-                    echo "Cleaning finished transmission items"
-                    $TRM $SERVER --torrent $TORRENTID --remove
-                elif ! [[ "$DL_COMPLETED" ]];  then
-                    echo "Torrent #$TORRENTID is not completed, ignoring. But who cares."
-                elif [[ ( "$STATE_SEEDING" || "$STATE_IDLE" ) ]]; then
-                    echo "torrent is seeding"
-                else echo "another unexpected condition, passing"
                 fi
             fi
         done
 fi
 
-if [[ $UNCOMPLETED -eq 0 ]];  then
-    :
-elif [[ $UNCOMPLETED -eq 1 ]];  then
-    echo "There's one torrent still downloading"
-else
-    echo "There's $UNCOMPLETED torrents still downloading"
-fi
-
-if [[ $SEEDING -eq 0 ]];  then
-    :
-elif [[ $SEEDING -eq 1 ]];  then
-    echo "There's one torrent currently seeding"
-else
-    echo "There's $SEEDING torrents currently seeding"
-fi
-
-if [[ $SORTER = true ]] && [[ $UNCOMPLETED -eq 0 ]] && [[ $SEEDING -eq 0 ]];  then
+if [[ $SORTER = true ]];  then
     echo "sorting things out"
     $FG execute --tasks sort
-else
-    :
 fi 
